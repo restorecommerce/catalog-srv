d5b6f4272f14e1b45f3aa23d77a335c2d2db5620
Vilsol <me@vil.so>
2021-04-20 17:45:50 +0300

    chore: add CI test

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
-  coverageReporters: ["json", "html"],
+  coverageReporters: ["json", "html", "lcov"],
   globals: {
     'ts-jest': {
       tsconfig: 'tsconfig.test.json'
